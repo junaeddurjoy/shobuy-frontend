1984d5c6870d11cd98cdabf0ccd842bff68ac168
junaeddurjoy <junaed.durjoy@gmail.com>
2024-05-18 22:50:50 +0600

    deploy

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -7,20 +7,38 @@ import Home from "../Pages/Home/Home";
 import AboutUs from "../Pages/AboutUs/AboutUs";
 import FAQs from "../Pages/FAQs/FAQs";
 export const router = createBrowserRouter([
+    // {
+    //     path: "/shobuy-frontend",
+    //     element: <Main></Main>,
+    //     children: [
+    //         {
+    //             path: "/shobuy-frontend",
+    //             element: <Home></Home>,
+    //         },
+    //         {
+    //             path: "/shobuy-frontend/aboutUs",
+    //             element: <AboutUs></AboutUs>,
+    //         },
+    //         {
+    //             path: "/shobuy-frontend/FAQs",
+    //             element: <FAQs></FAQs>,
+    //         }
+    //     ]
+    // },
     {
-        path: "/shobuy-frontend",
+        path: "/",
         element: <Main></Main>,
         children: [
             {
-                path: "/shobuy-frontend",
+                path: "/",
                 element: <Home></Home>,
             },
             {
-                path: "/shobuy-frontend/aboutUs",
+                path: "/aboutUs",
                 element: <AboutUs></AboutUs>,
             },
             {
-                path: "/shobuy-frontend/FAQs",
+                path: "/FAQs",
                 element: <FAQs></FAQs>,
             }
         ]
