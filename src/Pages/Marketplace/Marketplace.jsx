c2874a03e4f87093559bc1d8e5420cd20f3be708
junaeddurjoy <junaed.durjoy@gmail.com>
2024-05-21 20:25:32 +0600

    marketplace update

--- src/Pages/Marketplace/Marketplace.jsx
+++ src/Pages/Marketplace/Marketplace.jsx
@@ -36,14 +36,6 @@ const Marketplace = () => {
                         <img className="rounded-xl" src="https://i.ibb.co/LrB2SdX/marketplace-4.png" alt="" />
                     </div>
                 </div>
-                <div className="col-span-2 flex bg-[#DEFFE9] rounded-xl shadow-2xl m-5 ">
-                    <div className="pl-2 md:pl-5 lg:pl-5 pt-2 md:pt-5 lg:pt-5 absolute">
-                        <h1 className="text-left text-2xl md:text-4xl lg:text-4xl font-bold">Plan your dream wedding</h1>
-                    </div>
-                    <div className="relative items-end">
-                        <img className="mt-9 md:mt-[66px] lg:mt-6 h-[85%] lg:h-[95%] w-full ml-0 md:ml-[56px] lg:ml-0 rounded-xl" src="https://i.ibb.co/416F0bL/marketplace-6.png" alt="" />
-                    </div>
-                </div>
                 <div className="h-[30rem]  bg-[#E6E5FF] rounded-xl shadow-2xl m-5 ">
                     <div>
                         <img className="rounded-xl" src="https://i.ibb.co/ZV3f7mN/marketplace-5.png" alt="" />
@@ -60,6 +52,15 @@ const Marketplace = () => {
                         <h1 className="ml-5 text-left text-3xl md:text-4xl lg:text-3xl font-bold">Buy your gadgets</h1>
                     </div>
                 </div>
+                <div className="col-span-2 flex bg-[#DEFFE9] rounded-xl shadow-2xl m-5 ">
+                    <div className="pl-2 md:pl-5 lg:pl-5 pt-2 md:pt-5 lg:pt-5 absolute">
+                        <h1 className="text-left text-2xl md:text-4xl lg:text-4xl font-bold">Plan your dream wedding</h1>
+                    </div>
+                    <div className="relative items-end">
+                        <img className="mt-9 md:mt-[66px] lg:mt-6 h-[85%] lg:h-[95%] w-full ml-0 md:ml-[56px] lg:ml-0 rounded-xl" src="https://i.ibb.co/416F0bL/marketplace-6.png" alt="" />
+                    </div>
+                </div>
+
             </div>
         </div>
     );
